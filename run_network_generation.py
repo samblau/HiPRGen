e7890f71ee11257be88f8547d4f3f20e77749c7e
samblau <samblau1@gmail.com>
2024-01-07 14:08:10 -0800

    debugging

--- run_network_generation.py
+++ run_network_generation.py
@@ -24,17 +24,21 @@ grapher_features_dict_pickle_file = sys.argv[5]
 #wx,add reaction lmdb_path
 reaction_lmdb_path = sys.argv[6]
 
-
+print(rank, "loading mol_entries")
 with open(mol_entries_pickle_file, 'rb') as f:
     mol_entries = pickle.load(f)
 
+print(rank, "loading dgl_molecules_dict_pickle_file")
 with open(dgl_molecules_dict_pickle_file, 'rb') as f:
-    dgl_molecules_dict_pickle_file = pickle.load(f)
-for graph_i in dgl_molecules_dict_pickle_file:
-    dgl_molecules_dict_pickle_file[graph_i] =  load_dgl_graph_from_serialized(dgl_molecules_dict_pickle_file[graph_i])
+    dgl_molecules_dict = pickle.load(f)
 
+print(rank, "executing load_dgl_graph_from_serialized")
+for graph_i in dgl_molecules_dict:
+    dgl_molecules_dict[graph_i] = load_dgl_graph_from_serialized(dgl_molecules_dict[graph_i])
+
+print(rank, "loading grapher_features_dict_pickle_file")
 with open(grapher_features_dict_pickle_file, 'rb') as f:
-    grapher_features_dict_pickle_file = pickle.load(f)
+    grapher_features_dict = pickle.load(f)
 
 if rank == DISPATCHER_RANK:
     dispatcher_payload = loadfn(dispatcher_payload_json)
@@ -53,7 +57,7 @@ else:
     worker_payload = loadfn(worker_payload_json)
     worker(mol_entries,
            worker_payload,
-           dgl_molecules_dict_pickle_file,
-           grapher_features_dict_pickle_file,
+           dgl_molecules_dict,
+           grapher_features_dict,
            reaction_lmdb_path
            )
